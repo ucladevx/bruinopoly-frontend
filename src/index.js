b177afd2c89963a9f3f1771984e6844ab98d671d
shirschhorn9 <shirschhorn9@penguin>
2020-11-15 22:35:29 -0800

    took out unesscary import

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import { store } from './reducers';
 import { BrowserRouter as Router } from "react-router-dom";
 import {Provider} from 'react-redux'
-import './assets/VCR_OSD_MONO_1.001.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
